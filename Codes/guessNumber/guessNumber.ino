47dc9d23a8ee25d43aaf88e28c01d384a24d4b69
jason0342 <jasonwang924@hotmail.com>
2018-07-11 20:23:21 +0800

    finish guess number AI

--- Codes/guessNumber/guessNumber.ino
+++ Codes/guessNumber/guessNumber.ino
@@ -42,13 +42,27 @@ GameState gameState;
 struct GameData {
     char R[5];
     char guess[5];
+    bool histR[10];
+    bool histG[10];
     int A;
     int B;
     int count;
 } gameData;
 
+struct RevData {
+    bool possible[10000];
+    int guess;
+    int A;
+    int B;
+    int count;
+    int remain;
+} revData;
+
 void resetGuess() {
     strcpy(gameData.guess, "xxxx");
+    for(int i = 0; i < 10; ++i) {
+        gameData.histG[i] = 0;
+    }
     gameData.A = -1;
     gameData.B = -1;
 }
@@ -56,7 +70,41 @@ void resetGuess() {
 void resetGameData() {
     resetGuess();
     strcpy(gameData.R, "    ");
+    for(int i = 0; i < 10; ++i) {
+        gameData.histR[i] = 0;
+    }
     gameData.count = 0;
+}
+
+void resetRevGuess() {
+    revData.A = -1;
+    revData.B = -1;
+    revData.guess = 0;
+}
+
+void resetRevData() {
+    Serial.print("resetRevData()");
+    revData.remain = 10000;
+    // remove all invalid number
+    for(int i = 0; i < 10000; ++i) {
+        revData.possible[i] = 1;
+        bool hist[10] = {0};
+        int target = i;
+        for(int j = 0; j < 4; ++j) {
+            int digit = target % 10;
+            if(hist[digit]) {
+                revData.possible[i] = 0;
+                --revData.remain;
+                break;
+            }
+            hist[digit] = 1;
+            target /= 10;
+        }
+    }
+    Serial.print("remain: ");
+    Serial.print(revData.remain);
+    resetRevGuess();
+    revData.count = 0;
 }
 
 void setup() {
@@ -105,7 +153,7 @@ void drawStartScreen() {
     //Draw white frame
     tft.drawRect(0,0,319,240,WHITE);
     
-    tft.setCursor(25,100);
+    tft.setCursor(20,100);
     tft.setTextColor(WHITE);
     tft.setTextSize(4);
     tft.print("Guess Number");
@@ -138,17 +186,13 @@ void generate() {
     }
     int i = 0;
     int r;
-    bool alreadyExist;
     randomSeed(millis());
     while (i < 4) {
-        alreadyExist = false;
-        r = random(0, 10) + '0';
-        for (int j = 0; j < i; ++j) {
-          if (r == gameData.R[j]) alreadyExist = true;
-        }
-        if (!alreadyExist) {
-          gameData.R[i] = r;
-          ++i;
+        r = random(0, 10);
+        if (!gameData.histR[r]) {
+            gameData.R[i] = r + '0';
+            gameData.histR[r] = 1;
+            ++i;
         }
     }
     Serial.print("generate R: ");
@@ -174,9 +218,8 @@ void loop() {
             execGame();
         }
         else if(reverseButton.pressed(p.x, p.y)) {
-            // TODO
-            // revGameSetup();
-            // execRevGame();
+            revGameSetup();
+            execRevGame();
         }
     }
     if(gameState == endMode) {
@@ -227,9 +270,8 @@ void drawGameScreen() {
         drawHorizontalLine(112 + 64 * i - 2);
     }
     for(int i = 0; i < 10; ++i) {
-        printNumber(i);
+        if(!gameData.histG[i]) printNumber(i);
     }
-    // TODO: draw giveup button
     tft.setCursor(192 + 8, 0 + 8);
     tft.print("giveup");
     // draw guess
@@ -247,10 +289,10 @@ void drawGameScreen() {
         tft.setCursor(8, 74 + 8);
         tft.print("B: ");
         if(gameData.B >= 0) tft.print(gameData.B);
-        tft.setCursor(128 + 8, 74 + 8);
-        tft.print("count: ");
-        tft.print(gameData.count);
     }
+    tft.setCursor(128 + 8, 74 + 8);
+    tft.print("count: ");
+    tft.print(gameData.count);
 }
 
 void playerMove() {
@@ -274,9 +316,12 @@ void playerMove() {
             }
             for(int i = 0; i < 10; ++i) {
                 if(numButtons[i].pressed(p.x, p.y)) {
-                    gameData.guess[count] = '0' + i;
-                    ++count;
-                    drawGameScreen();
+                    if(!gameData.histG[i]) {
+                        gameData.guess[count] = '0' + i;
+                        gameData.histG[i] = 1;
+                        ++count;
+                        drawGameScreen();
+                    }
                     Serial.print("count = ");
                     Serial.print(count);
                     Serial.print("\tguess = ");
@@ -284,7 +329,6 @@ void playerMove() {
                     Serial.print("\n");
                 }
             }
-            // TODO draw guess
         }
         delay(50); // prevent wdt reset and continuous input
     }
@@ -295,21 +339,185 @@ void checkGuess() {
     Serial.print("checkGuess\n");
     gameData.A = 0;
     gameData.B = 0;
-    int hist1[10] = {0};
-    int hist2[10] = {0};
     for(int i = 0; i < 4; ++i) {
         if(gameData.R[i] == gameData.guess[i]) ++gameData.A;
-        ++hist1[gameData.R[i]-'0'];
-        ++hist2[gameData.guess[i]-'0'];
     }
     for(int i = 0; i < 10; ++i) {
-        if(hist1[i] && hist2[i]) ++gameData.B;
+        if(gameData.histR[i] && gameData.histG[i]) ++gameData.B;
     }
     gameData.B -= gameData.A;
 
     if(gameData.A == 4) {
         gameState = endMode;
         drawGameScreen();
+        delay(2000);
+    }
+    for(int i = 0; i < 10; ++i) {
+        gameData.histG[i] = 0;
+    }
+}
+
+// Reverse mode: AI guess your number
+
+void revGameSetup() {
+    giveupButton = ButtonCoordinate(192,0,128,56);
+    for (int i = 0; i < 10; ++i) {
+        numButtons[i] = ButtonCoordinate((i % 5) * 64, 112 + (i / 5) * 64, 64, 64);
+    }
+    resetRevData();
+    randomSeed(millis());
+    gameState = reverseMode;
+}
+
+void execRevGame() {
+    Serial.print("execRevGame");
+    while(gameState == reverseMode) {
+        AIMove();
+        drawRevScreen();
+        revPlayerMove();
+        AIFilter();
+    }
+}
+
+void drawRevScreen() {
+    tft.fillScreen(BLACK);
+    tft.drawRect(0,0,319,240,WHITE);
+    for(int i = 0; i < 4; ++i) {
+        drawVerticalLine(64 * (i+1) - 2);
+    }
+    for(int i = 0; i < 4; ++i) {
+        drawHorizontalLine(112 + 64 * i - 2);
+    }
+    for(int i = 0; i < 5; ++i) {
+        printNumber(i);
+    }
+    tft.setCursor(192 + 8, 0 + 8);
+    tft.print("back");
+    // draw guess
+    tft.setCursor(8, 8);
+    if(revData.remain > 0) {
+        if(revData.guess < 1000) tft.print("0");
+        tft.print(revData.guess);
+    }
+    else {
+        tft.print("Impossible!");
+    }
+    // draw result
+    if(revData.A == 4) {
+        tft.setCursor(8, 37 + 8);
+        tft.print("Game Over!");
+    }
+    else {
+        tft.setCursor(8, 37 + 8);
+        tft.print("A: ");
+        if(revData.A >= 0) tft.print(revData.A);
+        tft.setCursor(8, 74 + 8);
+        tft.print("B: ");
+        if(revData.B >= 0) tft.print(revData.B);
+    }
+    tft.setCursor(128 + 8, 74 + 8);
+    tft.print("count: ");
+    tft.print(revData.count);
+}
+
+void AIMove() {
+    // AI guess a number
+    resetRevGuess();
+    int index = random(0, 32767) % revData.remain;
+    int counter = 0;
+    for(int i = 0; i < 10000; ++i) {
+        if(revData.possible[i]) ++counter;
+        if(counter > index) {
+            revData.guess = i;
+            break;
+        }
+    }
+    ++revData.count;
+}
+
+void revPlayerMove() {
+    // player enter result
+    bool isTouched = false;
+    int count = 0;
+    while(count < 2) {
+        isTouched = ts.touched();
+        if(isTouched) {
+            isTouched = false;
+            TS_Point p0 = ts.getPoint();  //Get touch point
+            TS_Point p;
+            p.x = map(p0.x, TS_MINX, TS_MAXX, 0, 320);
+            p.y = map(p0.y, TS_MINY, TS_MAXY, 0, 240);
+            Serial.print("X = "); Serial.print(p.x);
+            Serial.print("\tY = "); Serial.print(p.y);
+            Serial.print("\n");
+            if(giveupButton.pressed(p.x, p.y)) {
+                gameState = endMode;
+                break;
+            }
+            for(int i = 0; i < 5; ++i) {
+                if(numButtons[i].pressed(p.x, p.y)) {
+                    if(count == 0) revData.A = i;
+                    else if(count == 1) revData.B = i;
+                    ++count;
+                    drawRevScreen();
+                    Serial.print("count = ");
+                    Serial.print(count);
+                    Serial.print("\tguess = ");
+                    Serial.print(gameData.guess);
+                    Serial.print("\n");
+                }
+            }
+            if(revData.A == 4) {
+                gameState = endMode;
+                delay(2000);
+                break;
+            }
+        }
+        delay(50); // prevent wdt reset and continuous input
+    }
+    ++gameData.count;
+}
+
+void AIFilter() {
+    Serial.print("filter\n");
+    // AI filters out possibilities by player result
+    if(gameState != reverseMode) return;
+    bool histG[10] = {0};
+    int guess[4] = {0};
+    int temp = revData.guess;
+    for(int i = 0; i < 4; ++i) {
+        guess[i] = temp % 10;
+        histG[temp % 10] = 1;
+        temp /= 10;
+    }
+    for(int i = 0; i < 10000; ++i) {
+        if(revData.possible[i]) {
+            bool histR[10] = {0};
+            int target[4] = {0};
+            int temp = i;
+            int A = 0;
+            int B = 0;
+            for(int j = 0; j < 4; ++j) {
+                target[j] = temp % 10;
+                histR[temp%10] = 1;
+                temp /= 10;
+            }
+            for(int j = 0; j < 4; ++j) {
+                if(guess[j] == target[j]) ++A;
+            }
+            for(int j = 0; j < 10; ++j) {
+                if(histG[j] && histR[j]) ++B;
+            }
+            B -= A;
+            if((A != revData.A) || (B != revData.B)) {
+                revData.possible[i] = 0;
+                --revData.remain;
+            }
+        }
+    }
+    if(revData.remain == 0) {
+        drawRevScreen();
+        gameState = endMode;
         delay(2000);
     }
 }
