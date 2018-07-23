908fd4d23db3f1dc8da12fc9cd5e5597911e0646
NTUwanderer <harveyycyang@gmail.com>
2018-07-23 15:30:41 +0800

    AStar: Add TODO_v1

---
+++ Codes/A_star/TODO_v1/myQueue.h
@@ -0,0 +1,34 @@
+#include <queue>
+#include <vector>
+
+#include "state.h"
+
+template<typename T,
+         typename Sequence = std::vector<T> >
+class myQueue : public std::priority_queue<T, Sequence, myHashComparison> {
+public:
+
+    bool edit(const T& value) {
+        for (auto it = this->c.begin(); it != this->c.end(); ++it) {
+            if (*it == value) {
+                this->c.erase(it);
+                // *it = value;
+                // std::make_heap(this->c.begin(), this->c.end(), this->comp);
+                this.push(value);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool remove(const T& value) {
+        for (auto it = this->c.begin(); it != this->c.end(); ++it) {
+            if (((*it << 28) >> 28) == ((value << 28) >> 28)) {
+                this->c.erase(it);
+                std::make_heap(this->c.begin(), this->c.end(), this->comp);
+                return true;
+            }
+        }
+        return false;
+    }
+};
